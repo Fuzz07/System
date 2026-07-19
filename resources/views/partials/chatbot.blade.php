5c99d1e6be798c23d6eacfda0bcb19d933971d97
Fuzz07 <davealagban9tle@gmail.com>
2026-07-19 23:02:51 +0800

    Update chatbot.blade.php

--- resources/views/partials/chatbot.blade.php
+++ resources/views/partials/chatbot.blade.php
@@ -108,7 +108,7 @@
   /* Chatbot Label */
   .chatbot-label {
     position: absolute;
-    right: 100px;
+    right: 500px;
     top: 50%;
     transform: translateY(-50%);
     background: var(--chatbot-primary-dark);
